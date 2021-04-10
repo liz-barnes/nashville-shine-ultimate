39ae19dabe5e6022319d1dc1e6dc37db9c405aa6
Liz Barnes <lizb@lyft.com>
2021-04-10 14:00:31 -0500

    setup navbar and routes

---
+++ src/Components/Roster/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Roster() {
+  return (
+    <div className="Roster">
+      <h2>Roster</h2>
+    </div>
+  );
+}
---
+++ src/Components/Schedule/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Schedule() {
+  return (
+    <div className="Schedule">
+      <h2>Schedule</h2>
+    </div>
+  );
+}
