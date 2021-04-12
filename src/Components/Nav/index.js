6826a66522b45f085179bbf68c3d110d77bfc966
Liz <liz@Liz-s-Mac.local>
2021-04-12 11:31:39 -0500

    design the layout using css grid

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -2,17 +2,22 @@ import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
-    <nav className="navbar">
-      <Link to='/' className='brand'>
-        'Shine
-      </Link>
-      <div className="navbar-items">
-        <Link to='/about' className='navbar-item'>about</Link>
-        <Link to='/roster' className='navbar-item'>roster</Link>
-        <Link to='/schedule' className='navbar-item'>schedule</Link>
-        <Link to='/highlights' className='navbar-item'>highlights</Link>
-        <Link to='/contact' className='navbar-item'>contact</Link>
-      </div>
-    </nav>
+    <div className='navbar-container'>
+      <header className='brand-container'>
+        <Link to='/' className='brand'>
+          <h1>'Shine</h1>
+        </Link>
+        <p>Nashville, TN</p>
+      </header>
+      <nav className='navbar'>
+        <div className='navbar-items'>
+          <Link to='/about' className='navbar-item'>about</Link>
+          <Link to='/roster' className='navbar-item'>roster</Link>
+          <Link to='/schedule' className='navbar-item'>schedule</Link>
+          <Link to='/highlights' className='navbar-item'>highlights</Link>
+          <Link to='/contact' className='navbar-item'>contact</Link>
+        </div>
+      </nav>
+    </div>
   );
 }
