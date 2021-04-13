d62fd379490c1daffefdb39b4c14441c2b2a119b
Liz Barnes <lizb@lyft.com>
2021-04-12 19:22:34 -0500

    playing around with div background image styling

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div className="Home">
-      <h2>Home</h2>
+    <div
+      className="Home"
+      style={{
+        backgroundImage: 'url(https://media.istockphoto.com/photos/young-man-catching-frisbee-picture-id182892188?k=6&m=182892188&s=612x612&w=0&h=eqV_ofr0RPub9iGo-yqIKvC9fy6oNiztLgeZ12b5pHw=)',
+      }}
+    >
+      <div className="Home" id='section'
+      style={{
+        backgroundImage: 'url(https://media.istockphoto.com/photos/young-man-catching-frisbee-picture-id182892188?k=6&m=182892188&s=612x612&w=0&h=eqV_ofr0RPub9iGo-yqIKvC9fy6oNiztLgeZ12b5pHw=)',
+      }}></div>
+      <div className='divider'></div>
+      <div className="Section" id='section'
+      style={{
+        backgroundImage: 'url(https://snworksceo.imgix.net/dpn/e5fe5d96-f866-49bc-b440-0fe282f43828.sized-1000x1000.jpg?w=1000)',
+      }}></div>
     </div>
   );
 }
