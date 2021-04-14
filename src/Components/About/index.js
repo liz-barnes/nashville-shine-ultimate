ec5afe31a32d23b94b120f42c5933ed5a6b2b451
Liz <liz@Liz-s-Mac.local>
2021-04-14 10:46:08 -0500

    spacing for the about page

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import fives from '../../helpers/images/fives.jpeg';
 
 export default function About() {
   return (
-    <div className="About">
-      <h2>About</h2>
+    <div id='about-page'>
+      <section id='about-us' className='page' style={{ backgroundImage: `url(${fives})` }}>
+          <h1 className='blurb'>Nashville Elite Mixed Ultimate Frisbee Club</h1>
+          <p className='SCF'>Spirit. Competition. Fun.</p>
+      </section>
+      <div id='shine-blurb' className='Section'>
+        <p class='shine-blurb'>
+          'Shine is a competitive, mixed club team based in Nashville,
+          Tennessee. We strive for a lasting culture of competition, integrity,
+          respect, fun, and development that benefits our players and improves
+          ultimate in the Nashville community.
+        </p>
+      </div>
     </div>
   );
 }
