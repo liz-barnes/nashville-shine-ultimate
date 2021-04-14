e462f6ca5b6361b41a2e84d9274c89b503ee0648
Liz <liz@Liz-s-Mac.local>
2021-04-14 12:13:42 -0500

    create roster 2019 data

---
+++ src/helpers/data/roster2019.js
@@ -0,0 +1,97 @@
+const roster2019 = [
+  {
+    name: 'aaron wilder (P)',
+  },
+  {
+    name: 'andy "barney" barnhart*',
+  },
+  {
+    name: 'audrey cheng',
+  },
+  {
+    name: 'cam snook',
+  },
+  {
+    name: 'chris "krim" calhoun',
+  },
+  {
+    name: 'claire bidigare-curtis',
+  },
+  {
+    name: 'colleen wright',
+  },
+  {
+    name: 'grant gardner',
+  },
+  {
+    name: 'jackie tingle',
+  },
+  {
+    name: 'jake wright',
+  },
+  {
+    name: 'jason bennett',
+  },
+  {
+    name: 'jeff burris',
+  },
+  {
+    name: 'jess cherayil (P)',
+  },
+  {
+    name: 'kelsey foster',
+  },
+  {
+    name: 'liz barnes',
+  },
+  {
+    name: 'matt ractliffe',
+  },
+  {
+    name: 'meaghan creamer',
+  },
+  {
+    name: 'mia letterie',
+  },
+  {
+    name: 'michael alberts (P)',
+  },
+  {
+    name: 'michael krohn*',
+  },
+  {
+    name: 'noel holmes*',
+  },
+  {
+    name: 'orion paul (P)',
+  },
+  {
+    name: 'reuben griggs',
+  },
+  {
+    name: 'richie salmon',
+  },
+  {
+    name: 'sadie jezierski',
+  },
+  {
+    name: 'sofia lima',
+  },
+  {
+    name: 'steven eakle',
+  },
+  {
+    name: 'tim krentz (P)',
+  },
+  {
+    name: 'trip adams',
+  },
+  {
+    name: 'zach "napkins" hipkens',
+  },
+  {
+    name: 'zack stern (P)',
+  },
+];
+
+export default roster2019;
