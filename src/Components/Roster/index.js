e629f70eb84fdb3cd8b900f084dac2b58112891d
Liz <liz@Liz-s-Mac.local>
2021-04-14 12:40:22 -0500

    style and space rosters

--- src/Components/Roster/index.js
+++ src/Components/Roster/index.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import sectionals from '../../helpers/images/sectionals.jpeg';
 import roster19 from '../../helpers/data/roster2019';
 
 export default function Roster() {
   return (
     <div id='roster-page' className='page'>
+      <div className='Section' style={{
+        backgroundImage: `url(${sectionals})`,
+      }}>
+      </div>
       <section>
-      </section>
-      <section>
-        <h2>the humans of 'shine</h2>
-        <div className='roster-container'>
-          <div className=''>
-            <h4>2019</h4>
+        <h2 className='roster-header'>the humans of 'shine</h2>
+        <div className='rosters-container'>
+        <div className='roster'>
+            <h4 className='year'>2021</h4>
+            <li>coming soon!</li>
+          </div>
+          <div className='roster'>
+            <h4 className='year'>2019</h4>
+            {roster19.map((player) => <li>{player.name}</li>)}
+          </div>
+          <div className='roster'>
+            <h4 className='year'>2018</h4>
             {roster19.map((player) => <li>{player.name}</li>)}
           </div>
         </div>
       </section>
-      <h2>Roster</h2>
     </div>
   );
 }
