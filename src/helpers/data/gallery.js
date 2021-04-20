9e093ae8eb14c41d4dcc56806c83aa99f93c3b14
Liz Barnes <lizb@lyft.com>
2021-04-20 11:01:50 -0500

    import react-photo-gallery, build out gallery data for react-photo-gallery

---
+++ src/helpers/data/gallery.js
@@ -0,0 +1,17 @@
+import huddle from '../images/huddle.jpeg';
+import plane from '../images/plane.jpeg';
+
+const gallery = [
+  {
+    src: { huddle },
+    width: 4,
+    height: 3,
+  },
+  {
+    src: { plane },
+    width: 1,
+    height: 1,
+  },
+];
+
+export default gallery;
