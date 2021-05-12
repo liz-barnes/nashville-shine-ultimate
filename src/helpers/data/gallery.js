87e4eabf8ce4a32a305a1fe3cb1170790a87289c
Liz <liz@Liz-s-Mac.local>
2021-05-12 09:45:51 -0500

    add images to highlights page

--- src/helpers/data/gallery.js
+++ src/helpers/data/gallery.js
@@ -20,9 +20,54 @@ const gallery = [
     height: 3,
   },
   {
+    src: 'images/shine-jeff-grab.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
     src: 'images/shine-audrey.jpg',
     width: 4,
     height: 3,
+  },
+  {
+    src: 'images/shine-cam-jump.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: 'images/shine-wacker.jpg',
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: 'images/shine-aaron.jpg',
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: 'images/shine-noel.jpg',
+    width: 4.5,
+    height: 3,
+  },
+  {
+    src: 'images/shine-steven.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: 'images/shine-circle.jpg',
+    width: 5,
+    height: 3.5,
+  },
+  {
+    src: 'images/shine-sadie2.jpg',
+    width: 5,
+    height: 3.5,
+  },
+  {
+    src: 'images/shine-trip-d.jpg',
+    width: 5,
+    height: 3.5,
   },
 ];
 
